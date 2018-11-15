fc9cffbea88f0b5b8ea1bdc7cc10492e4faf845e
tomoyk <tmyk.kym@gmail.com>
2018-11-15 22:17:47 +0900

    fixed calAdd.py

--- calAdd.py
+++ calAdd.py
@@ -23,27 +23,27 @@ def calAdd(posts):
           'summary': '[%s限] %s(%s, %s)' % (p['time'], p['course'], p['class'], p['teacher']),
           'description': p['misc'],
           'start': {
-            'dateTime': p['day']+'T09:00:00-07:00',
+            'date': p['day'],
             'timeZone': 'Asia/Tokyo',
           },
           'end': {
-            'dateTime': p['day']+'T17:00:00-07:00',
+            'date': p['day'],
             'timeZone': 'Asia/Tokyo',
-          },
-          'reminders': {
-            'useDefault': False,
-            'overrides': [
-              {'method': 'email', 'minutes': 24 * 60},
-              {'method': 'popup', 'minutes': 10},
-            ],
           },
         }
 
-        CAL_ID = 'edu.teu.ac.jp_9qr6n1mgs5vq3mgpcl3fsj0958@group.calendar.google.com'
+        CAL_ID = 'm771ihgg5fehhb2103ek6md31g@group.calendar.google.com'
         result = service.events().insert(calendarId=CAL_ID, body=event).execute()
         print('Event created: %s' % (result.get('htmlLink')))
 
 
 if __name__ == '__main__':
-    main()
+    calAdd(posts = [{
+           'time': '2',
+           'course': 'SampleLecture',
+           'class': 'A',
+           'teacher': 'John Doe',
+           'misc': 'hello world',
+           'day': '2018-11-30'
+           }])
 
